30d969cf753e91845c8caa60ac6dc060940d832f
RADAJO2 <radajo2@oocl.com>
2021-08-04 22:14:12 +0800

    [Practice05][Joanna] feat: practice5test

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,28 @@
 package practice05;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass != null)
+        {
+            return MessageFormat.format(super.introduce() + " I am a Teacher. I teach Class {0}.", klass);
+        }
+        else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
 }
