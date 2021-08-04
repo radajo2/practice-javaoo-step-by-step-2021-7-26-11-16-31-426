05ad01e072bb22b2332b9a971f558948b386a8de
RADAJO2 <radajo2@oocl.com>
2021-08-04 22:12:49 +0800

    [Practice04][Joanna] feat: practice04test

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,24 @@
 package practice04;
 
+import java.text.MessageFormat;
+
 public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return MessageFormat.format("My name is {0}. I am {1} years old.", name, age);
+    }
 }
--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,20 @@
 package practice04;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return MessageFormat.format(super.introduce() + " I am a Student. I am at Class {0}.", klass);
+    }
 }
