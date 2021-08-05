253dd513c975b7767e06b3dd2de1e6ab8d4efe6a
RADAJO2 <radajo2@oocl.com>
2021-08-05 08:24:32 +0800

    [Pratice07][Joanna] feat: should_teacher_have_name_and_age_and_class

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,16 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+    public Klass getKlass() {
+        return klass;
+    }
 }
