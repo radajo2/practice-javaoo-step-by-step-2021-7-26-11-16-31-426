171f3d9cad5c7f997328a51544ca45e1a8e4d3f1
RADAJO2 <radajo2@oocl.com>
2021-08-09 00:45:23 +0800

    [Practice09][Joanna] feat: should_teacher_introduce_itself_with_which_classes_it_teaches - test and implementation

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,6 +1,8 @@
 package practice09;
 
+import java.text.MessageFormat;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class Teacher extends Person{
     private LinkedList<Klass> klass;
@@ -16,5 +18,16 @@ public class Teacher extends Person{
 
     public LinkedList<Klass> getClasses()  {
         return klass;
+    }
+
+    String teacher = super.introduce() + " " + "I am a Teacher. ";
+
+    public String introduce() {
+        if (klass != null)
+        {
+            return MessageFormat.format(teacher + "I teach Class {0}.", klass.stream().map(klassVal -> String.valueOf(klassVal.getNumber())).collect(Collectors.joining(", ")));
+        }
+        else
+            return teacher + "I teach No Class.";
     }
 }
