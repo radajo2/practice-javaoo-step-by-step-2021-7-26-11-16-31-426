d888e0622aa25da08913b0dbc469f32ec018dbfd
RADAJO2 <radajo2@oocl.com>
2021-08-09 00:24:54 +0800

    [Practice09][Joanna] feat: should_class_have_a_number - test and implement

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,6 +1,13 @@
 package practice09;
 
 public class Klass {
-    public Klass(int i) {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
