18637cae8ec559fa3f8bbc699039031fbc9d7bd0
RADAJO2 <radajo2@oocl.com>
2021-08-09 00:41:28 +0800

    [Practice09][Joanna] feat: should_teacher_have_name_and_age_and_classes - test and implementation

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -2,6 +2,7 @@ package practice09;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Klass {
     private int number;
@@ -27,6 +28,24 @@ public class Klass {
         else{
             System.out.print("It is not one of us.\n");
         }
+    }
+
+    public boolean isIn(Student student)
+    {
+        return this.equals(student.getKlass());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 
     public Student getLeader() {
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,20 @@
 package practice09;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public LinkedList<Klass> getClasses()  {
+        return klass;
+    }
 }
