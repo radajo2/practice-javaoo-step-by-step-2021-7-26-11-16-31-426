b9518ba00f81adca1ea82d951afe327e822e1dcc
RADAJO2 <radajo2@oocl.com>
2021-08-09 00:28:29 +0800

    [Practice09][Joanna] feat: should_class_not_assign_a_student_as_leader_when_student_is_not_a_member - test and implement

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,7 +1,12 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
     private int number;
+    private Student leader;
+    List<Student> students = new ArrayList<>();
 
     public Klass(int number) {
         this.number = number;
@@ -13,5 +18,18 @@ public class Klass {
 
     public String getDisplayName() {
         return "Class " + number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.contains(leader)){
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
     }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,18 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
